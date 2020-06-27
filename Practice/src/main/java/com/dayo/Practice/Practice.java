d0e54494e25c889c380a5ff6121fcc5ca6ee833a
Adedayo-1 <ojade@Adedayo>
2020-06-27 00:44:17 -0400

    Conditional statements

---
+++ Practice/src/main/java/com/dayo/Practice/Practice.java
@@ -0,0 +1,110 @@
+package com.dayo.Practice;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class Practice {
+	private static String summer2020;
+
+	public static void main(String[] args) {
+		/*
+		 * Scanner scanner = new Scanner(System.in); System.out.println("What is x :");
+		 * int x =scanner .nextInt(); System.out.println("X is:" + x);
+		 * 
+		 * 
+		 * 
+		 * 
+		 * System.out.println("What is y :"); int y=scanner .nextInt();
+		 * System.out.println("y is :" + y );
+		 * 
+		 * // 2*x + (y +3x+4y)-2x//
+		 * 
+		 * int xy = 2*x + (y+3*x * 4*y) - 2*x; ; System.out.println("The answer is " +
+		 * xy);
+		 */
+//Promocode and 10% discount//
+		/*
+		 * Scanner scanner = new Scanner(System.in);
+		 * System.out.println("What is Quantity x :"); int x = scanner.nextInt();
+		 * 
+		 * 
+		 * System.out.println("What is Price y ;"); int y = scanner.nextInt();
+		 * 
+		 * int c = x * y; double g =c - 0.1 *c;
+		 * 
+		 * String promocode; System.out.println("What is Promocode: "); promocode =
+		 * scanner.next();
+		 * 
+		 * String d = "summer2020"; if (c >= 200 && promocode.equalsIgnoreCase(d))
+		 * 
+		 * System.out.println("Final Price:$" + g); else {
+		 * 
+		 * System.out.println("Final Price: " + c); }
+		 */
+
+		// Grading Matrix//
+
+		int avg = 0;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("What is the Student's score ? ");
+		avg = scanner.nextInt();
+		if (avg >= 85 && avg <= 100) {
+			System.out.print("Student's score is A");
+		} else if (avg >= 70 && avg <= 85) {
+			System.out.print("Student's score is B");
+		} else if (avg >= 60 && avg <= 69) {
+			System.out.print("Student's score is C");
+		} else if (avg >= 50 && avg <= 59) {
+			System.out.print("Student's score is D");
+		} else if (avg >= 40 && avg <= 49) {
+			System.out.print("Student's score is E");
+		} else if (avg >= 0 && avg <= 39) {
+			System.out.print("Student's score is F");
+		}
+
+	}
+}
+
+		
+		
+		
+		// As a user I want to be able to see the total sum of items showing the name of items and its price//
+		/*
+		String[] itemNames  = new String[5];
+		int size = itemNames.length;
+		
+		int [] itemPrices = new int[itemNames.length];
+		
+		for (int i = 0; i<5
+				*/
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+		
---
+++ Practice/src/test/java/com/dayo/Practice/PracticeTest.java
@@ -0,0 +1,20 @@
+package com.dayo.Practice;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class PracticeTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
